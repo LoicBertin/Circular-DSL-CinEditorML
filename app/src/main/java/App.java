12e1949d70c6d3baaff6a6ea56b842ab898f7446
BERTIN Loïc <bil995o@gmail.com>
2021-02-24 08:36:23 +0100

    fixed autoCompletion

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -7,6 +7,8 @@ import javax.swing.text.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
@@ -34,6 +36,7 @@ public class App extends JFrame implements DocumentListener, ActionListener {
 
     private static enum Mode { INSERT, COMPLETION };
     private static final String COMMIT_ACTION = "commit";
+    private static final String REROLL_ACTION = "reroll";
     private Mode mode = Mode.INSERT;
 
     public App(){
@@ -87,7 +90,7 @@ public class App extends JFrame implements DocumentListener, ActionListener {
     public void setUpAutoCompletion(){
         dslArea.getDocument().addDocumentListener(this);
 
-        keyWords = new ArrayList<String>(5);
+        keyWords = new ArrayList<String>();
         importKeyWords();
 
         InputMap im = dslArea.getInputMap();
@@ -141,7 +144,8 @@ public class App extends JFrame implements DocumentListener, ActionListener {
         String prefix = content.substring(w + 1).toLowerCase();
         boolean founded = false;
         for(String temp : keyWords){
-            if(temp.toLowerCase().contains(prefix.toLowerCase())){
+            if(temp.toLowerCase().startsWith(prefix.toLowerCase()) && !founded){
+                System.out.println(temp);
                 founded = true;
                 // A completion is found
                 String completion = temp.substring(pos - w);
@@ -181,6 +185,7 @@ public class App extends JFrame implements DocumentListener, ActionListener {
 
         public void run() {
             appendToPane(dslArea, completion, Color.BLACK);
+            String[] temp = {completion};
             //dslArea.insert(completion, position);
             dslArea.setCaretPosition(position + completion.length());
             dslArea.moveCaretPosition(position);
